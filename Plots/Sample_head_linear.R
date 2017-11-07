c9a9bc701827af492ae43839f441bd290c5b4a6d
Adam Dinan <adam1989ie@gmail.com>
2017-11-07 16:30:08 +0000

    updates

--- Plots/Sample_head_linear.R
+++ Plots/Sample_head_linear.R
@@ -1,14 +1,27 @@
 bitmap("Sample_linear_ggg_xxx.jpg",type="jpeg",width=6.9,height=1.2+nnn*1,res=600,pointsize=12)
 # change "Sample" to your virus name, e.g. "HIV"
 
-region_starts = c(1)
-region_ends = c(7452) #exclude the poly-A tail
-r1len = region_ends[1] - region_starts[1]
 mar5 = 650
 mar3 = 800
-uORF_pos <- 1
+
 cc1 <- 1.0 #(relative) font size
 
+
+# annotate the genome
+genomelength = 8335
+cdsstart = c(621,2238,5777) #start positions of gens
+cdsend = c(2237,5837,7774) #end positions of genes
+cdsy = c(-1,-1,1) # y coordinates at which gene rectangles will be plotted
+cdsnames = c("gag","pol","env") # names of genes
+namey = c(-1,-1,1) # y positions of gene names
+
+#define the plotting region (by default, the whole genome)
+region_starts = c(1)
+region_ends = c(genomelength)
+
+r1len = region_ends[1] - region_starts[1]
+
+#initialize plot layout
 layout(matrix(c(1:(3*(nnn+2))), nnn+2, 3, byrow = TRUE), c(mar5,r1len,mar3), c(0.5,1+0*c(1:nnn),0.01))
 
 par(mar=c(0,0,0,0))
@@ -20,14 +33,6 @@ cc1 = 0.7
 cc2 = 0.7
 cc3 = 0.7
 col3 = colors()[96]
-
-# annotate the genome
-genomelength = 8335
-cdsstart = c(621,2238,5777) #start positions of gens
-cdsend = c(2237,5837,7774) #end positions of genes
-cdsy = c(-1,-1,1) # y coordinates at which gene rectangles will be plotted
-cdsnames = c("gag","pol","env") # names of genes
-namey = c(-1,-1,1) # y positions of gene names
 
 
 temp=c(0,1)
